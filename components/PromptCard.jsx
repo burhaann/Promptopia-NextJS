22dd3891c8f0c145488a977e36154d9cc9e50e82
Burhaann <burhaann321@gmail.com>
2023-11-17 12:24:57 +0530

    Fixed Profile Click

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -25,8 +25,11 @@ const PromptCard = ({ post, handleEdit, handleDelete, handleTagClick }) => {
   };
 
   return (
-    <div className="prompt_card" onClick={handleProfileClick}>
-      <div className="flex justify-between items-start gap-5">
+    <div className="prompt_card">
+      <div
+        className="flex justify-between items-start gap-5"
+        onClick={handleProfileClick}
+      >
         <div className="flex-1 flex justify-start items-center gap-3 cursor-pointer">
           <Image
             src={post.creator.image}
